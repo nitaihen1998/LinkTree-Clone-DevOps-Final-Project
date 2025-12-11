195692b0363d177c9a24fe9d5d8c3273ca5ddd01
Or Levov <orlevov02@gmail.com>
2025-12-11 21:33:09 +0200

    nginx

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // API Configuration
-const API_BASE_URL = 'http://a411fe54f09334d17aa6668e2ff29942-341253149.us-east-1.elb.amazonaws.com:5000/api';
+const API_BASE_URL = '/api';
 
 export default API_BASE_URL;
