f65cd4c0c72db6c90be7fe4194175798060aa541
Or Levov <or@dormotech.com>
2026-01-14 22:24:49 +0200

    trigger

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -24,3 +24,6 @@ resource "aws_iam_openid_connect_provider" "eks" {
   thumbprint_list = [data.tls_certificate.oidc.certificates[0].sha1_fingerprint]
   url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
+
+
+ 
